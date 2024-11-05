37b324cad4d2115c23e87adf2f87e01f2a5dd03f
Christian Rodriguez <christianrodriguez@ChristiansAir8.lan>
2024-11-04 17:18:49 -0700

    Added the rules box

--- public/blackjack/blackjack.js
+++ public/blackjack/blackjack.js
@@ -7,7 +7,25 @@ function setupPlayerFunctions() {
 }
 
 function setupGameFunctions() {
+    const rulesButton = document.querySelector(".rules-button");
+    const rulesModal = document.getElementById("rulesModal");
+    const closeButton = document.querySelector(".close-button");
 
+    rulesButton.addEventListener("click", () => {
+        rulesModal.style.display = "flex"; // Display as flex to center content
+    });
+
+    // Hide the modal when the close button is clicked
+    closeButton.addEventListener("click", () => {
+        rulesModal.style.display = "none";
+    });
+
+    // Hide the modal when clicking outside of the modal content
+    window.addEventListener("click", (event) => {
+        if (event.target === rulesModal) {
+            rulesModal.style.display = "none";
+        }
+    });
 }
 
 
