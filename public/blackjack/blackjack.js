3f4285800b6022218ddf664fdfeee8c46be2a587
Christian Rodriguez <christianrodriguez@ChristiansAir8.lan>
2024-11-04 19:47:18 -0700

    deck fix

--- public/blackjack/blackjack.js
+++ public/blackjack/blackjack.js
@@ -3,7 +3,21 @@ function setupDealerFunctions() {
 }
 
 function setupPlayerFunctions() {
-
+    const deckContainer = document.getElementById('deckContainer');
+    const addCardButton = document.getElementById('addCardButton');
+    
+    let cardCount = 0; // To keep track of the number of cards
+    
+    addCardButton.addEventListener('click', () => {
+        cardCount++; // Increment card count
+    
+        // Create a new card element
+        const newCard = document.createElement('div');
+        newCard.classList.add('deck-box'); // Add the deck box class
+    
+        // Append the new card to the deck container
+        deckContainer.appendChild(newCard);
+    });
 }
 
 function setupGameFunctions() {
